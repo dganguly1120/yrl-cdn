fbe5d0667752a0dbeaec91eae9ea66b9b8ceae21
dganguly1120 <debayan@tomilkieway.com>
2021-07-10 02:47:36 +0530

    fix conversion script

--- conversion.js
+++ conversion.js
@@ -12,7 +12,7 @@ const yrl_conversion = (content={}) => {
   let cookies = decodeURIComponent(document.cookie).split(';');
   let refId = "";
   for(let i = 0; i <cookies.length; i++){
-      let c = ca[i];
+      let c = cookies[i];
       while (c.charAt(0) == ' ') {
           c = c.substring(1);
       }
