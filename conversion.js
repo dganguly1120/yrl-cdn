18c56f6fb4be02c459198f657d01e4ef4ee017e8
dganguly1120 <debayan@tomilkieway.com>
2021-07-16 22:18:57 +0530

    Delete cookie on conversion and set 7 day expiry.

--- conversion.js
+++ conversion.js
@@ -3,8 +3,10 @@ const setCookie = () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const y = urlParams.get("yrl_y");
+    const d = (new Date()).getTime() + 1000*60*60*24*7;
+    const expires = "expires="+(new Date(d)).toUTCString();
   if (y) 
-      document.cookie = `__yrl_referral=${y}`;
+      document.cookie = `__yrl_referral=${y};`+expires;
 }
 setCookie();
 const yrl_conversion = (content={}) => {
@@ -23,7 +25,7 @@ const yrl_conversion = (content={}) => {
 
   return new Promise(async (resolve, reject) => {
       try {
-          const rawResponse = await fetch(`https://dev.yrl.is/conversion/`, {
+          const rawResponse = await fetch(`https://yrl.is/conversion/`, {
               method: "POST",
               headers: {
                   Accept: "application/json",
@@ -32,6 +34,7 @@ const yrl_conversion = (content={}) => {
               body: JSON.stringify({ yrl: refId, content: content }),
           });
           const resp = await rawResponse.json();
+          document.cookie = `__yrl_referral=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`
           resolve("success");
       } catch (err) {
         console.log("something went wrong: ", err);
